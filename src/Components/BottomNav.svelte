f93ae9fdf739658e7ab13c2ca89066114c983543
valike23 <ludike23@gmail.com>
2022-12-08 23:11:13 +0100

    init

---
+++ src/Components/BottomNav.svelte
@@ -0,0 +1,30 @@
+<div class="tabbar bottom-nav pos-fixed">
+  <label class="tabbar__item">
+    <input type="radio" name="tabbar-a" checked="checked">
+    <button class="tabbar__button text-center">
+      <ion-icon class="tabbar__icon" name="heart"></ion-icon>
+      <div class="tabbar__label">One</div>
+    </button>
+  </label>
+
+  <label class="tabbar__item">
+    <input type="radio" name="tabbar-a">
+    <button class="tabbar__button">
+      <i class="tabbar__icon ion-ios-radio-button-on"></i>
+      <div class="tabbar__label">Two</div>
+    </button>
+  </label>
+
+  <label class="tabbar__item">
+    <input type="radio" name="tabbar-a">
+    <button class="tabbar__button">
+      <i class="tabbar__icon ion-ios-star"></i>
+      <div class="tabbar__label">Three</div>
+    </button>
+  </label>
+</div>
+
+<style>
+
+  
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,53 @@
+<script>
+  import { onMount } from "svelte";
+  import BottomNav from "../Components/BottomNav.svelte";
+  onMount(()=>{
+    const splide = new Splide( '.splide' ,{type: 'loop', perPage: 1}).mount();
+  })
+</script>
+<div class="container-fluid"  style="overflow-y: scroll;">
+  
+
+  <section class="splide" aria-labelledby="carousel-heading">
+  
+    <div class="splide__track">
+      <ul class="splide__list">
+        <li class="splide__slide">
+          <div class="card bg-green">
+            <div class="card__content ">
+              <p style="font-weight: 700;" class="text-center fg-white text-700 text-light">₦5,000</p>
+            </div>
+          </div>
+        </li>
+        <li class="splide__slide">
+          <div class="card bg-green">
+          <div class="card__content ">
+            <p style="font-weight: 700;" class="text-center fg-white text-700 text-light">₦5,000</p>
+          </div>
+        </div></li>
+        <li class="splide__slide">
+          <div class="card bg-green">
+          <div class="card__content ">
+            <p style="font-weight: 700;" class="text-center fg-white text-700 text-light">₦5,000</p>
+          </div>
+        </div></li>
+      </ul>
+    </div>
+  </section>
+
+    
+    <div class="mt-5 mb-5"> there is a content here</div>
+    <div class="mt-5 mb-5"> there is a content here</div>
+    <div class="mt-5 mb-5"> there is a content here</div>
+    <div class="mt-5 mb-5"> there is a content here</div>
+    <div class="mt-5 mb-5"> there is a content here</div>
+    <div class="mt-5 mb-5"> there is a content here</div>
+    <div class="mt-5 mb-5"> there is a content here</div>
+    <div class="mt-5 mb-5"> there is a content2 here</div>
+
+
+</div>
+
+<BottomNav/>
+
+
